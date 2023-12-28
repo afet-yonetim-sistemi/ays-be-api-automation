70a29a60636827c8da4ef75c7f0497787013547d
cglais <caglaataseven1988@gmail.com>
2023-12-28 21:23:00 +0300

    AYS-70 | Positive and Negative test added for GetAdminRegistrationApplicationsIDTest.java class. This issue completed.

--- src/test/java/endpoints/InstitutionEndpoints.java
+++ src/test/java/endpoints/InstitutionEndpoints.java
@@ -2,10 +2,7 @@ package endpoints;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import payload.Assignment;
-import payload.RequestBodyAssignments;
-import payload.RequestBodyInstitution;
-import payload.User;
+import payload.*;
 
 import static io.restassured.RestAssured.given;
 
@@ -84,6 +81,14 @@ public class InstitutionEndpoints {
                 .when()
                 .post(Routes.postCreateAssignment);
     }
+    public static Response createAnInstitution(Institution institution) {
+        return given()
+                .header("Authorization", "Bearer " + Authorization.superAdminAuthorization())
+                .contentType(ContentType.JSON)
+                .body(institution)
+                .when()
+                .post(Routes.postRegistrationApplications);
+    }
 
     public static Response getAssignment(String assignmentId) {
         return given()
@@ -114,24 +119,37 @@ public class InstitutionEndpoints {
                 .delete(Routes.deleteAssignment);
     }
 
-    public static Response postRegistrationApplications(RequestBodyInstitution requestBodyInstitution){
-        return given()
-                .header("Authorization" , "Bearer " + Authorization.superAdminAuthorization())
-                .contentType(ContentType.JSON)
-                .body(requestBodyInstitution)
-                .when()
-                .post(Routes.postRegistrationApplications);
-    }
-
-    public static Response listAdmins(RequestBodyInstitution requestBodyInstitution) {
+    public static Response postRegistrationApplications(RequestBodyInstitution requestBodyInstitution, String userType) {
+        if (userType.equals("SUPER_ADMIN")) {
+            return given()
+                    .header("Authorization", "Bearer " + Authorization.superAdminAuthorization())
+                    .contentType(ContentType.JSON)
+                    .body(requestBodyInstitution)
+                    .when()
+                    .post(Routes.postRegistrationApplications);
+        }
         return given()
                 .header("Authorization", "Bearer " + Authorization.institutionAuthorization())
                 .contentType(ContentType.JSON)
                 .body(requestBodyInstitution)
                 .when()
                 .post(Routes.postRegistrationApplications);
+
     }
 
+    public static Response getRegistrationApplicationsId(String institutionID) {
 
-
+        return given()
+                .header("Authorization", "Bearer " + Authorization.superAdminAuthorization())
+                .pathParam("id", institutionID)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(Routes.getRegistrationApplicationsWithId);
+    }
 }
+
+
+
+
+
+
--- src/test/java/tests/institution/adminregistrationmanagementservice/GetAdminRegistrationApplicationIDTest.java
+++ src/test/java/tests/institution/adminregistrationmanagementservice/GetAdminRegistrationApplicationIDTest.java
@@ -1,4 +1,57 @@
 package tests.institution.adminregistrationmanagementservice;
 
+import endpoints.InstitutionEndpoints;
+import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import payload.Helper;
+import payload.Institution;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.notNullValue;
+
 public class GetAdminRegistrationApplicationIDTest {
+    Logger logger = LogManager.getLogger(this.getClass());
+    String institutionID;
+
+
+    @Test()
+    public void getInstitutionPositive() {
+        logger.info("ARMS_17 is running");
+        institutionID = "3cfeb994-88f4-48d0-b105-309f42f91412";
+        Response response = InstitutionEndpoints.getRegistrationApplicationsId(institutionID);
+        response.then()
+                .log().body()
+                .statusCode(200)
+                .contentType("application/json")
+                .body("time", notNullValue())
+                .body("isSuccess", equalTo(true))
+                .body("httpStatus", equalTo("OK"))
+                .body("response.createdAt", notNullValue())
+                .body("response.id", notNullValue())
+                .body("response.createdUser", notNullValue())
+                .body("response.status", notNullValue())
+                .body("response.adminUser.firstName", notNullValue())
+                .body("response.adminUser.lastName", notNullValue())
+                .body("response.adminUser.email", notNullValue())
+                .body("response.adminUser.id", notNullValue())
+                .body("response.adminUser.phoneNumber", nullValue());
+    }
+    @Test()
+    public void getInstitutionInvalidId() {
+        logger.info("ARMS_18 is running");
+        institutionID = "invalid-id";
+        Response response = InstitutionEndpoints.getRegistrationApplicationsId(institutionID);
+        response.then()
+                .log().body()
+                .statusCode(400)
+                .contentType("application/json")
+                .body("time", notNullValue())
+                .body("isSuccess", equalTo(false))
+                .body("httpStatus", equalTo("BAD_REQUEST"))
+                .body("header",equalTo("VALIDATION ERROR"))
+                .body("subErrors[0].message", containsString("must be a valid UUID"));
+    }
 }
