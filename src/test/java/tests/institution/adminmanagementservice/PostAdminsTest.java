b6f6f5e9714e86dbbe1d3de9b8b5190522b1d504
Nbhtozcn <nebahatasam@hotmail.com>
2024-01-04 00:31:56 -0500

    Allure annotations integration for PostAdminsTest.java class

--- src/test/java/tests/institution/adminmanagementservice/PostAdminsTest.java
+++ src/test/java/tests/institution/adminmanagementservice/PostAdminsTest.java
@@ -1,153 +1,198 @@
 package tests.institution.adminmanagementservice;
-
 import endpoints.InstitutionEndpoints;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.response.Response;
+import io.restassured.specification.ResponseSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
+import java.util.List;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.equalTo;
-
+@Epic("Institution Admin Management Service")
 public class PostAdminsTest {
     Logger logger = LogManager.getLogger(this.getClass());
     String currentPage;
-    String pageSize;
+    String currentPageSize;
 
-
-    @Test()
-    public void listAdminsWithValidPageAndPageSize() {
+    @BeforeMethod
+    public void setup() {
+        currentPage = null;
+        currentPageSize = null;
+    }
+    @Test(dataProvider = "positivePaginationData")
+    @Story("As an Institution admin I want to list all admins")
+    @Severity(SeverityLevel.NORMAL)
+    public void listAdminsWithValidPageAndPageSize(int page, int pageSize) {
         logger.info("Test case IMS_01 is running");
-        currentPage = "1";
-        pageSize = "10";
-        String pagination = "{\"pagination\":{\"page\":" + currentPage + ",\"pageSize\":" + pageSize + "}}";
+        String pagination = "{\"pagination\":{\"page\":" + page + ",\"pageSize\":" + pageSize + "}}";
         Response response = InstitutionEndpoints.listAdmins(pagination);
-        response.then()
-                .statusCode(200)
-                .contentType("application/json")
-                .body("response.content", hasSize(greaterThan(0)))
-                .body("response.content[0].id", notNullValue())
-                .body("response.content[0].username", notNullValue())
-                .body("response.content[0].firstName", notNullValue())
-                .body("response.content[0].lastName", notNullValue())
-                .body("response.content[0].status", notNullValue())
-                .body("response.pageNumber", notNullValue())
-                .body("response.pageSize", notNullValue())
-                .body("response.totalPageCount", notNullValue())
-                .body("response.totalElementCount", notNullValue())
-                .body("response", hasKey("sortedBy"))
-                .body("response", hasKey("filteredBy"));
+        if (page == 1 && pageSize == 10) {
+            response.then()
+                    .spec(successResponseSpec())
+                    .body("response.content", hasSize(greaterThan(0)))
+                    .body("response.content[0].id", notNullValue())
+                    .body("response.content[0].username", notNullValue())
+                    .body("response.content[0].firstName", notNullValue())
+                    .body("response.content[0].lastName", notNullValue())
+                    .body("response.content[0].status", notNullValue())
+                    .body("response.pageNumber", equalTo(page))
+                    .body("response.totalPageCount", notNullValue())
+                    .body("response.totalElementCount", notNullValue())
+                    .body("response", hasKey("sortedBy"))
+                    .body("response", hasKey("filteredBy"));
+        } else {
+            response.then()
+                    .spec(successResponseSpec())
+                    .body("response.content", instanceOf(List.class))
+                    .body("response.pageNumber", equalTo(page))
+                    .body("response.totalPageCount", notNullValue())
+                    .body("response.totalElementCount", notNullValue())
+                    .body("response", hasKey("sortedBy"))
+                    .body("response", hasKey("filteredBy"));
+        }
     }
 
     @Test()
+    @Story("As an Institution admin I want to get proper error message when page value is null")
+    @Severity(SeverityLevel.NORMAL)
     public void listAdminsWithEmptyPageValue() {
         logger.info("Test case IMS_02 is running");
-        pageSize = "10";
-        String pagination = "{\"pagination\":{\"page\":\"\",\"pageSize\":" + pageSize + "}}";
+        currentPageSize = "10";
+        String pagination = "{\"pagination\":{\"page\":" + currentPage + ",\"pageSize\":" + currentPageSize + "}}";
         Response response = InstitutionEndpoints.listAdmins(pagination);
         response.then()
-                .statusCode(400)
-                .contentType("application/json")
-                .body("time", notNullValue())
-                .body("httpStatus", equalTo("BAD_REQUEST"))
-                .body("header", equalTo("VALIDATION ERROR"))
-                .body("isSuccess", equalTo(false))
-                .body("subErrors[0].message", equalTo("must not be null"))
+                .spec(badRequestResponseSpec())
+                .body("subErrors[0].message", equalTo("must be between 1 and 99999999"))
                 .body("subErrors[0].field", equalTo("page"))
-                .body("subErrors[0].type", equalTo("Long"));
+                .body("subErrors[0].type", equalTo("int"));
     }
 
     @Test()
+    @Story("As an Institution admin I want to get proper error message when pageSize value is null")
+    @Severity(SeverityLevel.NORMAL)
     public void listAdminsWithEmptyPageSizeValue() {
         logger.info("Test case IMS_03 is running");
         currentPage = "1";
-        String pagination = "{\"pagination\":{\"page\":" + currentPage + ",\"pageSize\":" + pageSize + "}}";
+        String pagination = "{\"pagination\":{\"page\":" + currentPage + ",\"pageSize\":" + currentPageSize + "}}";
         Response response = InstitutionEndpoints.listAdmins(pagination);
         response.then()
-                .statusCode(400)
-                .contentType("application/json")
-                .body("time", notNullValue())
-                .body("httpStatus", equalTo("BAD_REQUEST"))
-                .body("header", equalTo("VALIDATION ERROR"))
-                .body("isSuccess", equalTo(false))
-                .body("subErrors[0].message", equalTo("must not be null"))
+                .spec(badRequestResponseSpec())
+                .body("subErrors[0].message", equalTo("must be between 1 and 99999999"))
                 .body("subErrors[0].field", equalTo("pageSize"))
-                .body("subErrors[0].type", equalTo("Long"));
+                .body("subErrors[0].type", equalTo("int"));
     }
 
     @Test()
+    @Story("As an Institution admin I want to get proper error message when page field is missing")
+    @Severity(SeverityLevel.NORMAL)
     public void listAdminsWithMissingPageField() {
         logger.info("Test case IMS_04 is running");
-        pageSize = "10";
-        String pagination = "{\"pagination\":{\"pageSize\":" + pageSize + "}}";
+        currentPageSize = "10";
+        String pagination = "{\"pagination\":{\"pageSize\":" + currentPageSize + "}}";
         Response response = InstitutionEndpoints.listAdmins(pagination);
         response.then()
-                .statusCode(400)
-                .contentType("application/json")
-                .body("time", notNullValue())
-                .body("httpStatus", equalTo("BAD_REQUEST"))
-                .body("header", equalTo("VALIDATION ERROR"))
-                .body("isSuccess", equalTo(false))
-                .body("subErrors[0].message", equalTo("must not be null"))
+                .spec(badRequestResponseSpec())
+                .body("subErrors[0].message", equalTo("must be between 1 and 99999999"))
                 .body("subErrors[0].field", equalTo("page"))
-                .body("subErrors[0].type", equalTo("Long"));
+                .body("subErrors[0].type", equalTo("int"));
     }
 
     @Test()
+    @Story("As an Institution admin I want to get proper error message when pageSize field is missing")
+    @Severity(SeverityLevel.NORMAL)
     public void listAdminsWithMissingPageSizeField() {
         logger.info("Test case IMS_05 is running");
         currentPage = "1";
         String pagination = "{\"pagination\":{\"page\":" + currentPage + "}}";
         Response response = InstitutionEndpoints.listAdmins(pagination);
         response.then()
-                .statusCode(400)
-                .contentType("application/json")
-                .body("time", notNullValue())
-                .body("httpStatus", equalTo("BAD_REQUEST"))
-                .body("header", equalTo("VALIDATION ERROR"))
-                .body("isSuccess", equalTo(false))
-                .body("subErrors[0].message", equalTo("must not be null"))
+                .spec(badRequestResponseSpec())
+                .body("subErrors[0].message", equalTo("must be between 1 and 99999999"))
                 .body("subErrors[0].field", equalTo("pageSize"))
-                .body("subErrors[0].type", equalTo("Long"));
+                .body("subErrors[0].type", equalTo("int"));
     }
 
     @Test()
+    @Story("As an Institution admin I want to get proper error message when page and pageSize fields are missing")
+    @Severity(SeverityLevel.NORMAL)
     public void listAdminsWithMissingPageAndPageSizeField() {
         logger.info("Test case IMS_06 is running");
         String pagination = "{\"pagination\":{}}";
         Response response = InstitutionEndpoints.listAdmins(pagination);
         response.then()
-                .statusCode(400)
-                .contentType("application/json")
-                .body("time", notNullValue())
-                .body("httpStatus", equalTo("BAD_REQUEST"))
-                .body("header", equalTo("VALIDATION ERROR"))
-                .body("isSuccess", equalTo(false))
-                .body("subErrors[0].message", equalTo("must not be null"))
-                .body("subErrors[0].field", equalTo("pageSize"))
-                .body("subErrors[0].type", equalTo("Long"))
-                .body("subErrors[1].message", equalTo("must not be null"))
-                .body("subErrors[1].field", equalTo("page"))
-                .body("subErrors[1].type", equalTo("Long"));
+                .spec(badRequestResponseSpec())
+                .body("subErrors[0].message", equalTo("must be between 1 and 99999999"))
+                .body("subErrors[0].field", anyOf(equalTo("page"), equalTo("pageSize")))
+                .body("subErrors[0].type", equalTo("int"))
+                .body("subErrors[1].message", equalTo("must be between 1 and 99999999"))
+                .body("subErrors[1].field", anyOf(equalTo("page"), equalTo("pageSize")))
+                .body("subErrors[1].type", equalTo("int"));
     }
 
-    @Test()
-    public void listAdminsWithPageSizeValueNot10() {
+    @Test(dataProvider = "negativePaginationData")
+    @Story("As an Institution admin I want to get proper error message when page or pageSize fields are invalid")
+    @Severity(SeverityLevel.NORMAL)
+    public void listAdminsWithNegativeScenarios(int page, int pageSize) {
         logger.info("Test case IMS_07 is running");
-        currentPage = "1";
-        pageSize = "4";
-        String pagination = "{\"pagination\":{\"page\":" + currentPage + ",\"pageSize\":" + pageSize + "}}";
+        String pagination = "{\"pagination\":{\"page\":" + page + ",\"pageSize\":" + pageSize + "}}";
         Response response = InstitutionEndpoints.listAdmins(pagination);
         response.then()
-                .statusCode(400)
-                .contentType("application/json")
-                .body("time", notNullValue())
-                .body("httpStatus", equalTo("BAD_REQUEST"))
-                .body("header", equalTo("VALIDATION ERROR"))
-                .body("isSuccess", equalTo(false))
-                .body("subErrors[0].message", equalTo("must be between 10 and 10"))
-                .body("subErrors[0].field", equalTo("pageSize"))
-                .body("subErrors[0].type", equalTo("Long"));
+                .spec(badRequestResponseSpec())
+                .body("subErrors[0].message", equalTo("must be between 1 and 99999999"))
+                .body("subErrors[0].field", anyOf(equalTo("page"), equalTo("pageSize")))
+                .body("subErrors[0].type", equalTo("int"));
+    }
+
+    private ResponseSpecification badRequestResponseSpec() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(400)
+                .expectContentType("application/json")
+                .expectBody("time", notNullValue())
+                .expectBody("httpStatus", equalTo("BAD_REQUEST"))
+                .expectBody("header", equalTo("VALIDATION ERROR"))
+                .expectBody("isSuccess", equalTo(false))
+                .build();
+    }
+
+    private ResponseSpecification successResponseSpec() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType("application/json")
+                .expectBody("time", notNullValue())
+                .expectBody("httpStatus", equalTo("OK"))
+                .expectBody("isSuccess", equalTo(true))
+                .build();
+    }
+
+    @DataProvider(name = "negativePaginationData")
+    public static Object[][] negativePaginationData() {
+        return new Object[][]{
+                {-1, 10},
+                {100000000, 10},
+                {-5, 10},
+                {1, -10},
+                {1, 1000000000},
+                {1, -5},
+                {-100, 100000000},
+                {-5, -5},
+                {100000000, 100000000}
+        };
+    }
+
+    @DataProvider(name = "positivePaginationData")
+    public static Object[][] positivePaginationData() {
+        return new Object[][]{
+                {1, 10},
+                {99999999, 1},
+                {50, 50},
+                {1000, 20}
+        };
     }
 
 }
