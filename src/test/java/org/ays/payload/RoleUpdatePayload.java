454b5d8c19091a237e983261b1d669e458ceeeb0
agitrubard <demiragitrubar@gmail.com>
2024-09-03 18:38:10 +0300

    AYS-347 | `RoleUpdatePayload.generate()` Methods Have Been Removed

--- src/test/java/org/ays/payload/RoleUpdatePayload.java
+++ src/test/java/org/ays/payload/RoleUpdatePayload.java
@@ -12,17 +12,4 @@ public class RoleUpdatePayload {
     private String name;
     private List<String> permissionIds;
 
-    public static RoleUpdatePayload generate(RoleCreatePayload roleCreatePayload) {
-        RoleUpdatePayload roleUpdatePayload = new RoleUpdatePayload();
-        roleUpdatePayload.setName(roleCreatePayload.getName());
-        roleUpdatePayload.setPermissionIds(roleCreatePayload.getPermissionIds());
-        return roleUpdatePayload;
-    }
-
-    public static RoleUpdatePayload generate() {
-        RoleUpdatePayload roleUpdatePayload = new RoleUpdatePayload();
-        roleUpdatePayload.setName(RoleCreatePayload.generate().getName());
-        roleUpdatePayload.setPermissionIds(RoleCreatePayload.generate().getPermissionIds());
-        return roleUpdatePayload;
-    }
 }
