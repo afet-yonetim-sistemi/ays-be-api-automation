adab5e14bbad27f5d98040371b9f7451155249fb
agitrubard <demiragitrubar@gmail.com>
2024-09-11 18:30:08 +0300

    AYS-441 | `EmergencyEvacuationApplicationsFilter` Class Has Been Moved in `EmergencyEvacuationApplicationListPayload` Class

--- src/test/java/org/ays/payload/EmergencyEvacuationApplicationListPayload.java
+++ src/test/java/org/ays/payload/EmergencyEvacuationApplicationListPayload.java
@@ -3,10 +3,15 @@ package org.ays.payload;
 import lombok.Getter;
 import lombok.Setter;
 import org.ays.common.model.payload.AysPageable;
+import org.ays.utility.DatabaseUtility;
+
+import java.util.Collections;
+import java.util.List;
 
 @Getter
 @Setter
 public class EmergencyEvacuationApplicationListPayload {
+
     private AysPageable pageable;
     private EmergencyEvacuationApplicationsFilter filter;
 
@@ -16,4 +21,36 @@ public class EmergencyEvacuationApplicationListPayload {
         emergencyEvacuationApplicationListPayload.setFilter(EmergencyEvacuationApplicationsFilter.generate(emergencyEvacuationApplication));
         return emergencyEvacuationApplicationListPayload;
     }
+
+    @Getter
+    @Setter
+    public static class EmergencyEvacuationApplicationsFilter {
+
+        private String referenceNumber;
+        private String sourceCity;
+        private String sourceDistrict;
+        private int seatingCount;
+        private PhoneNumber phoneNumber;
+        private String targetCity;
+        private String targetDistrict;
+        private List<String> statuses;
+        private Boolean isInPerson;
+
+        public static EmergencyEvacuationApplicationsFilter generate(EmergencyEvacuationApplication emergencyEvacuationApplication) {
+            EmergencyEvacuationApplicationsFilter filter = new EmergencyEvacuationApplicationsFilter();
+            filter.setReferenceNumber(DatabaseUtility.getLatestReferenceNumber());
+            filter.setPhoneNumber(emergencyEvacuationApplication.getPhoneNumber());
+            filter.setSourceCity(emergencyEvacuationApplication.getSourceCity());
+            filter.setSourceDistrict(emergencyEvacuationApplication.getSourceDistrict());
+            filter.setSeatingCount(emergencyEvacuationApplication.getSeatingCount());
+            filter.setTargetCity(emergencyEvacuationApplication.getTargetCity());
+            filter.setTargetDistrict(emergencyEvacuationApplication.getTargetDistrict());
+            filter.setStatuses(Collections.singletonList("PENDING"));
+            filter.setIsInPerson(true);
+            return filter;
+        }
+
+    }
+
+
 }
