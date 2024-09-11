28d529f17c7604ac98cfd83be3de8500054bcdc6
agitrubard <demiragitrubar@gmail.com>
2024-09-11 17:44:25 +0300

    AYS-441 | Permission Endpoints Have Been Separated from `InstitutionEndpoints` Class and `PermissionEndpoints` Class Has Been Integrated to `GetPermissionsTest` Class

---
+++ src/test/java/org/ays/auth/permission/endpoints/PermissionEndpoints.java
@@ -0,0 +1,35 @@
+package org.ays.auth.permission.endpoints;
+
+import io.restassured.response.Response;
+import lombok.experimental.UtilityClass;
+import org.ays.common.model.payload.AysRestAssuredPayload;
+import org.ays.common.util.AysRestAssured;
+import org.ays.endpoints.Authorization;
+import org.openqa.selenium.remote.http.HttpMethod;
+
+@UtilityClass
+public class PermissionEndpoints {
+
+    public static Response getAdminsPermissions() {
+
+        AysRestAssuredPayload restAssuredPayload = AysRestAssuredPayload.builder()
+                .httpMethod(HttpMethod.GET)
+                .url("/api/v1/permissions")
+                .token(Authorization.loginAndGetAdminAccessToken())
+                .build();
+
+        return AysRestAssured.perform(restAssuredPayload);
+    }
+
+    public static Response getSuperAdminsPermissions() {
+
+        AysRestAssuredPayload restAssuredPayload = AysRestAssuredPayload.builder()
+                .httpMethod(HttpMethod.GET)
+                .url("/api/v1/permissions")
+                .token(Authorization.loginAndGetSuperAdminAccessToken())
+                .build();
+
+        return AysRestAssured.perform(restAssuredPayload);
+    }
+
+}
