b55104235c7fbc21ee8069bd88bb888561850089
cglais <caglaataseven1988@gmail.com>
2024-09-22 12:21:12 +0300

    AYS-441 | Parameters name refactored.

--- src/test/java/org/ays/common/util/AysConfigurationProperty.java
+++ src/test/java/org/ays/common/util/AysConfigurationProperty.java
@@ -34,36 +34,36 @@ public class AysConfigurationProperty {
         public static final String URL = PROPERTIES.getProperty("database.url");
         public static final String USERNAME = PROPERTIES.getProperty("database.username");
         public static final String PASSWORD = PROPERTIES.getProperty("database.password");
-        public static final String VOLUNTEER_FOUNDATION_ID = PROPERTIES.getProperty("volunteer_foundation_id");
-        public static final String AFET_YONETIM_SISTEMI_ID = PROPERTIES.getProperty("afet_yonetim_sistemi_id");
-        public static final String DISASTER_FOUNDATION_ID = PROPERTIES.getProperty("disaster_foundation_id");
-        public static final String TEST_FOUNDATION_ID = PROPERTIES.getProperty("test_foundation_id");
+        public static final String VOLUNTEER_FOUNDATION_ID = PROPERTIES.getProperty("volunteer_foundation.id");
+        public static final String AFET_YONETIM_SISTEMI_ID = PROPERTIES.getProperty("afet_yonetim_sistemi.id");
+        public static final String DISASTER_FOUNDATION_ID = PROPERTIES.getProperty("disaster_foundation.id");
+        public static final String TEST_FOUNDATION_ID = PROPERTIES.getProperty("test_foundation.id");
     }
 
     public static class SuperAdminUserOne {
-        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("afet_yonetim_sistemi_admin.email_address");
-        public static final String PASSWORD = PROPERTIES.getProperty("afet_yonetim_sistemi_admin.password");
+        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("afet_yonetim_sistemi.admin.email_address");
+        public static final String PASSWORD = PROPERTIES.getProperty("afet_yonetim_sistemi.admin.password");
     }
 
     public static class AdminUserOne {
-        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("volunteer_foundation_admin.email_address");
-        public static final String PASSWORD = PROPERTIES.getProperty("volunteer_foundation_admin.password");
+        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("volunteer_foundation.admin.email_address");
+        public static final String PASSWORD = PROPERTIES.getProperty("volunteer_foundation.admin.password");
     }
 
     public static class AdminUserTwo {
-        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("disaster_foundation_admin.email_address");
-        public static final String PASSWORD = PROPERTIES.getProperty("disaster_foundation_admin.password");
+        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("disaster_foundation.admin.email_address");
+        public static final String PASSWORD = PROPERTIES.getProperty("disaster_foundation.admin.password");
     }
 
     public static class TestAdmin {
-        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("test_foundation_admin.email_address");
-        public static final String PASSWORD = PROPERTIES.getProperty("test_foundation_admin.password");
+        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("test_foundation.admin.email_address");
+        public static final String PASSWORD = PROPERTIES.getProperty("test_foundation.admin.password");
     }
 
 
     public static class LandingUserOne {
-        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("landing_page_user_martha.email_address");
-        public static final String PASSWORD = PROPERTIES.getProperty("landing_page_user_martha.password");
+        public static final String EMAIL_ADDRESS = PROPERTIES.getProperty("disaster_foundation.user.email_address");
+        public static final String PASSWORD = PROPERTIES.getProperty("disaster_foundation.user.password");
     }
 
     private static class AysConfigurationException extends RuntimeException {
