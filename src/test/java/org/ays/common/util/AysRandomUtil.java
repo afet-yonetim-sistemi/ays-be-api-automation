823ce7f4b9b98093512f3a5bacd09a803f939644
Nbhtozcn <nebahatasam@hotmail.com>
2025-05-15 14:00:32 -0400

    AYS-842 | Refactored e-mail address generator method

--- src/test/java/org/ays/common/util/AysRandomUtil.java
+++ src/test/java/org/ays/common/util/AysRandomUtil.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import lombok.experimental.UtilityClass;
 
 import java.util.Random;
+import java.util.UUID;
 
 @UtilityClass
 public class AysRandomUtil {
@@ -22,8 +23,9 @@ public class AysRandomUtil {
     public String generateEmailAddress() {
         final String firstName = FAKER.name().firstName().toLowerCase().replaceAll("[^a-z]", "");
         final String lastName = FAKER.name().lastName().toLowerCase().replaceAll("[^a-z]", "");
+        final String uuid = UUID.randomUUID().toString().replaceAll("[^a-zA-Z0-9]", "").substring(0, 10).toLowerCase();
         final String prefix = "test";
-        return (prefix + firstName + "." + lastName + "@afetyonetimsistemi.test");
+        return prefix + firstName + "." + lastName + "_" + uuid + "@afetyonetimsistemi.test";
     }
 
     public static String generatePassword() {
